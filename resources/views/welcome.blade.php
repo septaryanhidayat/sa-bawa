0064a808064facd69f8373eb3ca4586f273f110d
Septa Ryan Hidayat <ryan.hidayah@gmail.com>
2026-07-23 19:13:43 +0700

    Update kode dari local Herd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>SA'BAWA - Silvi Aryanti' Badminton Assessment WebApp</title>
+    <link rel="icon" type="image/png" href="/images/logo1.png">
     
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
