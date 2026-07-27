18e6fc550c187304934e23f18aa0d0a952aebb91
septaryanhidayat <ryan.hidayah@gmail.com>
2026-07-27 13:17:30 +0700

    ganti nama universitas sriwijaya

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -131,7 +131,7 @@
                         <span class="bg-purple-100 text-purple-800 font-extrabold text-xs px-3 py-1 rounded-full uppercase tracking-wider">
                             Aplikasi Resmi Asesmen Bulutangkis
                         </span>
-                        <span class="text-xs text-slate-500 font-medium">FKIP Penjaskes Sriwijaya</span>
+                        <span class="text-xs text-slate-500 font-medium">FKIP Penjaskes Universitas Sriwijaya</span>
                     </div>
 
                     <h2 class="text-3xl font-black text-slate-900 tracking-tight leading-tight" x-text="appSettings.heroTitle"></h2>
